cbbc337dca73eb580162f0f08a5571ca080d8013
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2024-09-18 11:17:52 -0700

    made base template for frontend

--- static/js/measurement_log.js
+++ static/js/measurement_log.js
@@ -1,22 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('profile_id') === null) {
+        window.location.href = '/login';
+    }
     const about = document.getElementById('about');
     const login = document.getElementById('login');
     const tutorial = document.getElementById('tutorial');
     const home = document.getElementById('home');
-    tutorial.disabled = true;
     about.addEventListener('click', (event) => {
         window.location.href = '/about';
     });
-    if (localStorage.getItem('profile_id') !== null) {
-        login.textContent = 'PROFILE';
-        login.addEventListener('click', (event) => {
-            window.location.href = '/profile';
-        });
-    } else {
-        login.addEventListener('click', (event) => {
-            window.location.href = '/login';
-        });
-    }
+    login.textContent = 'PROFILE';
+    login.addEventListener('click', (event) => {
+        window.location.href = '/profile';
+    });
     tutorial.addEventListener('click', (event) => {
         window.location.href = '/tutorial';
     });
